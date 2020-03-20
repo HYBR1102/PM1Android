67118143fa84912f41698e42454f8a05fdf35875
HYBR1102 <54300096+hybr1102@users.noreply.github.com>
2020-03-20 09:24:41 -0500

    Menú contextual

--- app/src/main/java/com/example/introduccion/KotlinMain2Activity.kt
+++ app/src/main/java/com/example/introduccion/KotlinMain2Activity.kt
@@ -3,18 +3,71 @@ package com.example.introduccion
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
-import android.view.Menu
-import android.view.MenuInflater
-import android.view.MenuItem
-import android.view.View
+import android.view.*
+import android.widget.AdapterView
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_kotlin_main2.*
 
 class KotlinMain2Activity : AppCompatActivity() {
 
+    var actionMode : ActionMode? = null
+
+    private val actionModeCallback = object : ActionMode.Callback {
+        // Called when the action mode is created; startActionMode() was called
+        override fun onCreateActionMode(mode: ActionMode, menu: Menu): Boolean {
+            // Inflate a menu resource providing context menu items
+            val inflater: MenuInflater = mode.menuInflater
+            inflater.inflate(R.menu.menu_main2, menu)
+            return true
+        }
+
+        // Called each time the action mode is shown. Always called after onCreateActionMode, but
+        // may be called multiple times if the mode is invalidated.
+        override fun onPrepareActionMode(mode: ActionMode, menu: Menu): Boolean {
+            return false // Return false if nothing is done
+        }
+
+        // Called when the user selects a contextual menu item
+        override fun onActionItemClicked(mode: ActionMode, item: MenuItem): Boolean {
+            return when (item.itemId) {
+                R.id.kmoneda -> {
+                    startActivity(Intent(applicationContext, KotlinMainActivity::class.java))
+                    mode.finish() // Action picked, so close the CAB
+                    true
+                }
+                R.id.jmoneda -> {
+                    startActivity(Intent(applicationContext, MainActivity::class.java))
+                    mode.finish() // Action picked, so close the CAB
+                    true
+                }
+                else -> false
+            }
+        }
+
+        // Called when the user exits the action mode
+        override fun onDestroyActionMode(mode: ActionMode) {
+            actionMode = null
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_kotlin_main2)
+        //Menú contextual flotante------------------------------------------------------------------
+        //registerForContextMenu(tvLong)
+
+        tvLong.setOnLongClickListener { view ->
+            // Called when the user long-clicks on someView
+            when (actionMode) {
+                null -> {
+                    // Start the CAB using the ActionMode.Callback defined above
+                    actionMode = startActionMode(actionModeCallback)
+                    view.isSelected = true
+                    true
+                }
+                else -> false
+            }
+        }
     }
 
     fun ConversorMoneda(view: View) {
@@ -47,7 +100,7 @@ class KotlinMain2Activity : AppCompatActivity() {
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         val inflater: MenuInflater = menuInflater
-        inflater.inflate(R.menu.menu_main2, menu)
+        //inflater.inflate(R.menu.menu_main2, menu)
         return true
     }
 
@@ -63,6 +116,27 @@ class KotlinMain2Activity : AppCompatActivity() {
                 true
             }
             else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    override fun onCreateContextMenu(menu: ContextMenu, v: View,
+                                     menuInfo: ContextMenu.ContextMenuInfo?) {
+        super.onCreateContextMenu(menu, v, menuInfo)
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.menu_main2, menu)
+    }
+
+    override fun onContextItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.kmoneda -> {
+                startActivity(Intent(this, KotlinMainActivity::class.java))
+                true
+            }
+            R.id.jmoneda -> {
+                startActivity(Intent(this, MainActivity::class.java))
+                true
+            }
+            else -> super.onContextItemSelected(item)
         }
     }
 }
